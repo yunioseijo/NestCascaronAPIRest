34b53a827408d87647c3959601d9878e3b060f01
yunioseijo <yunioseijo@gmail.com>
2025-09-05 16:30:11 +0200

    agrego los módulos de common messages-ws y auth , todos vacíos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,42 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { CommonModule } from './common/common.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod' ? { rejectUnauthorized: false } : null,
+      },
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT ?? 5432),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    CommonModule,
+    AuthModule,
+
+    MessagesWsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
